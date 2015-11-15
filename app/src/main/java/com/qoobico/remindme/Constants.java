fa2dc528f024ebcc73d4e187d63e264399eaa1c6
MobDevK-Ra <rakiri1697@gmail.com>
2015-11-15 19:08:16 +0200

    Create MainActivity class and layout

---
+++ app/src/main/java/com/qoobico/remindme/Constants.java
@@ -0,0 +1,11 @@
+package com.qoobico.remindme;
+
+/**
+ * Created by Кирилл on 15.11.2015.
+ */
+public class Constants {
+
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+}
